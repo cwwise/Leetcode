abba6b0ac9174893d82b700a286f1a4943ef0e39
chenwei <wei18810109052@163.com>
2020-05-25 23:31:22 +0800

    update

--- Leetcode_C++/Leetcode_C++/Solution164.hpp
+++ Leetcode_C++/Leetcode_C++/Solution164.hpp
@@ -11,23 +11,90 @@
 
 #include <stdio.h>
 
-class DoubleLinkList {
-    
+struct DLinkedNode {
+    int key, value;
+    DLinkedNode* prev;
+    DLinkedNode* next;
+    DLinkedNode(): key(0), value(0), prev(nullptr), next(nullptr) {}
+    DLinkedNode(int _key, int _value): key(_key), value(_value), prev(nullptr), next(nullptr) {}
 };
 
 class LRUCache {
+    int capacity;
+    int size;
+    unordered_map<int, DLinkedNode *> map;
+    // 头节点和尾节点
+    DLinkedNode *head;
+    DLinkedNode *tail;
 public:
-    LRUCache(int capacity) {
-        
+    LRUCache(int _capacity) {
+        capacity = _capacity;
+        size = 0;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
     }
     
     int get(int key) {
-        return 1;
+        if (!map.count(key)) {
+            return -1;
+        }
+        DLinkedNode *node = map[key];
+        moveToHead(node);
+        return node->value;
     }
     
     void put(int key, int value) {
+        
+        if (!map.count(key)) {
+            // 如果 key 不存在，创建一个新的节点
+            DLinkedNode* node = new DLinkedNode(key, value);
+            map[key] = node;
+            insertToHead(node);
+            size ++;
 
+            // 先判断是否超过范围
+            if (size > capacity) {
+                // 移除最后的
+                DLinkedNode *node = removeTail();
+                map.erase(node->key);
+                size --;
+            }
+        } else {
+            DLinkedNode *node = map[key];
+            node->value = value;
+            moveToHead(node);
+        }
     }
+
+    // 移除
+    void removeNode(DLinkedNode * node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    // 移到头部
+    void moveToHead(DLinkedNode * node) {
+        removeNode(node);
+        insertToHead(node);
+    }
+    
+    DLinkedNode* removeTail() {
+        DLinkedNode *node = tail->prev;
+        tail->prev = node->prev;
+        node->prev->next = tail;
+        return node;
+    }
+    
+    // 添加头
+    void insertToHead(DLinkedNode *node) {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
+
 };
 
 #endif /* Solution164_hpp */
